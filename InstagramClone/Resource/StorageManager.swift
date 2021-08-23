94a52a209c9c2dcfa591a1d495a777b175f5c07b
jdr4343 <jdr4343@naver.com>
2021-08-24 02:52:30 +0900

    Storage Manager

--- InstagramClone/Resource/StorageManager.swift
+++ InstagramClone/Resource/StorageManager.swift
@@ -5,11 +5,11 @@
 //  Created by 신지훈 on 2021/08/24.
 //
 //사용자가 사진이나 비디오를 업로드 하여 firebase 클라우드 저장했을때 사용할 저장소 관리자를 만들어줍니다.
-import firebases
+import FirebaseStorage
 
-public class DatabaseManger {
+public class StorageManager {
     
-    static let shared = DatabaseManger()
+    static let shared = StorageManager()
     
    
 }
