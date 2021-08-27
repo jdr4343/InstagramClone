3f218c1386a3b8d4f911400ce4e94c0087b015e4
jdr4343 <jdr4343@naver.com>
2021-08-27 16:44:05 +0900

    UserListView

--- InstagramClone/Controller/General/ListViewController.swift
+++ InstagramClone/Controller/General/ListViewController.swift
@@ -11,7 +11,7 @@ class ListViewController: UIViewController {
 
     private let tableView: UITableView = {
         let table = UITableView()
-        
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         return table
     }()
     
@@ -26,14 +26,41 @@ class ListViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
         
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+}
+extension ListViewController: UITableViewDataSource,UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let model = data[indexPath.row]
+        
+    }
+    
     
 }
---
+++ InstagramClone/Views/Cell/UserFollowTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  UserFollowTableViewCell.swift
+//  InstagramClone
+//
+//  Created by 신지훈 on 2021/08/27.
+//
+
+import UIKit
+
+class UserFollowTableViewCell: UITableViewCell {
+
+    static let identifier = "UserFollowTableViewCell"
+    
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.font = .systemFont(ofSize: 17, weight: .regular)
+        return label
+    }()
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.clipsToBounds = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
