ded930699a4020f6af0e7f9b8231c62e3f3326be
jdr4343 <jdr4343@naver.com>
2021-08-28 12:10:57 +0900

    Position

--- InstagramClone/Controller/CoreTabs/HomeViewController.swift
+++ InstagramClone/Controller/CoreTabs/HomeViewController.swift
@@ -35,6 +35,7 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        view.addSubview(tableView)
         createMockModel()
     }
     
